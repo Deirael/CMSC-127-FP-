1cd5ed3a5480fd3033c549a49bc6e155a9de86d1
Geez-l <93700998+Geez-l@users.noreply.github.com>
2023-06-19 17:07:34 +0800

    Update Final Paper.sql

--- Final Paper.sql
+++ Final Paper.sql
@@ -1,5 +1,5 @@
 
-Find the names, departments, and email of all the advisers who uploaded research abstracts from the month of May
+/* Find the names, departments, and email of all the advisers who uploaded research abstracts from the month of May
 
 
 Find the research id, title, and abstract with a bookmark greater than 50.
@@ -8,4 +8,7 @@ Find the names and email addresses of all authors who are affiliated with the de
 Find the names and contact information of all academic organizations whose department heads have the permission role of "Admin"
 Find the names and email addresses of all advisers who have uploaded research papers and have contact information that ends with '.edu'
 Find the names, titles, and publication dates of the latest 5 research papers
-Find the average number of upvotes for all research papers. 
+Find the average number of upvotes for all research papers.  */
+
+
+
